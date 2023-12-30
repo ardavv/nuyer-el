10f016dba3e75d18102f2a38bf6ae24bf9ce8015
ardav <111423884+ardavv@users.noreply.github.com>
2023-12-30 19:33:15 +0700

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Particles from "react-particles";
+import {loadFireworksPreset} from "tsparticles-preset-fireworks"
+import { Typewriter } from "react-simple-typewriter";
+import { useState, useEffect } from "react";
+import Countdown from "react-countdown";
+
 
 function App() {
+  const [newYearMessage, setNewYearMessage] = useState([
+    "Hai",
+    "Happy New Year Ye!\n I love You"
+  ]);
+
+  const particleInit = async (engine) => {
+    await loadFireworksPreset(engine);
+  };
+
+  function timeLeft() {
+    const newYearDate = new Date("January 1, 2024 00:00:00").getTime()
+    const nowDate = new Date().getTime()
+    const remainingTime = newYearDate - nowDate
+
+    return remainingTime
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles
+        init={particleInit}
+        options={{preset: "fireworks"}}
+      />
+
+      <div className="flex flex-col justify-center items-center min-h-screen gap-4">
+        <span className="text-white text-4xl font-bold px-4 z-50">
+          <Typewriter 
+          words={newYearMessage} 
+          loop={false}
+          cursorStyle={"🐽"}
+          cursor
+          deleteSpeed={10}
+          />
+        </span>
+        <div className="z-50 text-white font-bold text-2xl">
+          <Countdown date={Date.now() + timeLeft()} onComplete={() => setNewYearMessage([
+            "Selamat Tahun Baru El"
+          ])}/>
+        </div>
+      </div>
+
+    </>
   );
 }
 
