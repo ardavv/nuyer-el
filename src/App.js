476565219114199f42909d887e6a952c48050898
ardav <111423884+ardavv@users.noreply.github.com>
2023-12-31 23:58:23 +0700

    editing package

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import {Route, Routes} from 'react-router-dom'
 import Newyear from './Pages/Newyear'
 import Home from './Pages/Home'
 import Keren from './Pages/Keren'
-import Awal from './Pages/Awal'
 
 
 function App() {
   return (
     <>  
     <Routes>
-      <Route index element={<Awal/>}/>
-      <Route path='/home' element={<Awal/>} />
+      <Route index element={<Home/>}/>
       <Route path='/foto' element={<Home/>} />
       <Route path='/newyear' element={<Newyear/>} />
       <Route path='/kespes' element={<Keren/>} />
